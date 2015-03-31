e9142411db019ca84fb0141710e5f568fa8a8bf7
Eric Templeton <erictempleton1@gmail.com>
2015-03-31 07:56:19 -0400

    moved auth out of class

--- twitter_api.py
+++ twitter_api.py
@@ -1,9 +1,12 @@
 import tweepy
 import config
 
+auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
+auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+
 
 class TwitterConn(object):
 
     def __init__(self, end_point, limit=None):
-        auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
-        auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_TOKEN_SECRET)
+        
